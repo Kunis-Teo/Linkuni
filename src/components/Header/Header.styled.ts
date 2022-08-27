d815f7c25154a129ab5d4db90d7d066511666266
Woo KyungJun <natcho9010@gmail.com>
2022-08-27 23:28:49 +0900

    feat: Header component

---
+++ src/components/Header/Header.styled.ts
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { BsPersonCircle } from "react-icons/bs";
+
+export const Wrapper = styled.div`
+  position: sticky;
+  width: 100%;
+  height: 50px;
+`;
+
+export const PersonIconWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
