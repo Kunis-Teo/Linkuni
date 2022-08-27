2caade41db52655852abfe790a6d4e91f255a57c
Woo KyungJun <natcho9010@gmail.com>
2022-08-27 22:45:10 +0900

    feat: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+
+import SearchPage from "@/components/SearchPage/SearchPage";
+import { Wrapper } from "./App.styled";
 
 function App() {
   return (
-    <div>
-      asdf
-    </div>
+    <Wrapper>
+      <SearchPage />
+    </Wrapper>
   );
 }
 
