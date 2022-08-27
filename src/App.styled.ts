946fa68fd13b16ee0887bc4d803c151aaecf4728
Lee-jisang <dlwltkd2003@naver.com>
2022-08-28 00:54:21 +0900

    update

---
+++ src/App.styled.ts
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  width: 100%;
+  max-width: 600px;
+  height: 100%;
+`;
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./App.css";
+import "./Login.css";
 
 function App() {
   return (
