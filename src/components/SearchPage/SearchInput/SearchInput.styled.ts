c57ca641c8e9d12fb0f7bae1105c4abb26b85941
Woo KyungJun <natcho9010@gmail.com>
2022-08-27 23:30:32 +0900

    style: change SearchInput style

--- src/components/SearchPage/SearchInput/SearchInput.styled.ts
+++ src/components/SearchPage/SearchInput/SearchInput.styled.ts
@@ -4,7 +4,8 @@ import { AiOutlineSearch } from "react-icons/ai";
 export const Wrapper = styled.div`
   display: flex;
   align-items: center;
-  width: 300px;
+  width: 100%;
+  max-width: 300px;
   border-bottom: 1px black solid;
 `;
 
@@ -14,6 +15,7 @@ export const Input = styled.input`
   padding: 5px 10px;
   outline: none;
   border: none;
+  box-sizing: border-box;
 `;
 
 export const SearchIcon = styled(AiOutlineSearch)`
