2190fc3f6f5861c9ad11b68e7a948f2dbbae709c
Lee-jisang <dlwltkd2003@naver.com>
2022-08-27 22:33:20 +0900

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Login from "./login";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
-  <App />
+  <React.StrictMode>
+    <Login />
+    {/* console.log(axios); */}
+  </React.StrictMode>
 );
---
+++ src/login.tsx
@@ -0,0 +1,96 @@
+import React from "react";
+import "./App.css";
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="row full-height justify-content-center">
+          <div className="col-12 text-center align-self-center py-5">
+            <div className="section pb-5 pt-5 pt-sm-2 text-center">
+              <h6 className="mb-0 pb-3">
+                <span>Log In </span>
+                <span>Sign Up</span>
+              </h6>
+              <input
+                className="checkbox"
+                type="checkbox"
+                id="reg-log"
+                name="reg-log"
+              />
+              <label htmlFor="reg-log"></label>
+              <div className="card-3d-wrap mx-auto">
+                <div className="card-3d-wrapper">
+                  <div className="card-front">
+                    <div className="center-wrap">
+                      <div className="section text-center">
+                        <h4 className="mb-4 pb-3">Log In</h4>
+                        <div className="form-group">
+                          <input
+                            type="email"
+                            name="logemail"
+                            className="form-style"
+                            placeholder="Your id"
+                            id="logemail"
+                          />
+                          <i className="input-icon uil uil-at"></i>
+                        </div>
+                        <div className="form-group mt-2">
+                          <input
+                            type="password"
+                            name="logpass"
+                            className="form-style"
+                            placeholder="Your Password"
+                            id="logpass"
+                          />
+                          <i className="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <a href="#" className="btn mt-4">
+                          submit
+                        </a>
+                        <p className="mb-0 mt-4 text-center"></p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="card-back">
+                    <div className="center-wrap">
+                      <div className="section text-center">
+                        <h4 className="mb-4 pb-3">Sign Up</h4>
+                        <div className="form-group mt-2">
+                          <input
+                            type="email"
+                            name="logemail"
+                            className="form-style"
+                            placeholder="Enter your id"
+                            id="logemail"
+                          />
+                          <i className="input-icon uil uil-at"></i>
+                        </div>
+                        <div className="form-group mt-2">
+                          <input
+                            type="password"
+                            name="logpass"
+                            className="form-style"
+                            placeholder="Enter your Password"
+                            id="logpass"
+                          />
+                          <i className="input-icon uil uil-lock-alt"></i>
+                        </div>
+                        <a href="#" className="btn mt-4">
+                          submit
+                        </a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
