e49a6fdf2f0e725cc426839937d4251082f3dae6
Woo KyungJun <natcho9010@gmail.com>
2022-08-28 19:19:10 +0900

    style: UrlCardList component

--- src/components/UrlCardList/UrlCardList.tsx
+++ src/components/UrlCardList/UrlCardList.tsx
@@ -1,6 +1,7 @@
 import { URL } from "@/types";
-import UrlCard from "../UrlCard/UrlCard";
-import styled from "styled-components";
+import UrlCard from "@/components/UrlCard/UrlCard";
+
+import { StyledWrapper } from "./UrlCardList.styled";
 
 interface Props {
   cards: URL[];
@@ -21,29 +22,3 @@ const UrlCardList = ({ cards, categoryTitle }: Props) => {
 };
 
 export default UrlCardList;
-
-const StyledWrapper = styled.section`
-  width: 100%;
-  padding: 20px;
-  .section-title {
-    font-size: 24px;
-    font-weight: 600;
-    padding: 20px 0;
-  }
-  .card-list-wrapper {
-    padding: 0;
-    height: 100%;
-    display: flex;
-    flex-wrap: nowrap;
-
-    width: 100%;
-    overflow-x: auto;
-
-    &::-webkit-scrollbar {
-      display: none;
-    }
-    & > :not(:last-child) {
-      margin-right: 10px;
-    }
-  }
-`;
