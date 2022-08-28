73a1ac8d56c992a554f6cc3d9f063dc359bef508
roberto6385 <roberto@netand.co.kr>
2022-08-28 14:51:17 +0900

    chore : merger request 를위한 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import LinkForm from "./components/LinkForm/index";
 function App() {
   return (
     <>
-      <LinkForm linkData={null} />
+      {/*<LinkForm linkData={null} />*/}
     </>
   );
 }
