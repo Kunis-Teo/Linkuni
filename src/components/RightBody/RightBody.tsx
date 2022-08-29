fb985ec284756d770a363735d77f51c5a695febc
Woo KyungJun <natcho9010@gmail.com>
2022-08-29 21:47:43 +0900

    feat: kangaroo & addbtn

--- src/components/RightBody/RightBody.tsx
+++ src/components/RightBody/RightBody.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
 
 import Header from "@/components/Header/Header";
 import SearchPage from "@/components/SearchPage/SearchPage";
@@ -6,19 +6,29 @@ import TagList from "@/domains/tag/List";
 import LinkForm from "@/components/LinkForm";
 import UrlCardList from "@/components/UrlCardList/UrlCardList";
 import { mockCardData } from "@/mocks/mockCardData";
-import { SearchPageWrapper, AddButton } from "./RightBody.styled";
+import kangaroo from "@/assets/kangaroo.png";
+import addbtn from "@/assets/addbtn.png";
+import {
+  SearchPageWrapper,
+  AddButton,
+  KangarooImage,
+} from "./RightBody.styled";
 
 function RightBody() {
   const [modalOpen, setModalOpen] = useState(false);
+
+  const handleAddButtonClick = useCallback(() => {
+    setModalOpen(true);
+  }, []);
 
   return (
     <>
       <Header />
       <SearchPageWrapper>
-        <img src="kangaroo.png" />
+        <KangarooImage src={kangaroo} />
         <SearchPage />
         <TagList />
-        <AddButton src="addbtn.png" />
+        <AddButton src={addbtn} onClick={handleAddButtonClick} />
       </SearchPageWrapper>
       <LinkForm linkData={null} visible={modalOpen} setVisible={setModalOpen} />
       <UrlCardList categoryTitle="프론트엔드" cards={mockCardData} />
